8eb3caefb5d10a6b32d05a457adaeb3f400af692
Katerina Chrysostomou Nygren <katerina.chrysostomou@hotmail.com>
2022-09-30 13:27:27 +0200

    changed logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,15 +7,15 @@ import "../styles/header.css";
 function Header() {
   return (
     <header>
-      <img
+      {/* <img
         src="https://see.fontimg.com/api/renderfont4/BW0ox/eyJyIjoiZnMiLCJoIjo5OCwidyI6MTUwMCwiZnMiOjY1LCJmZ2MiOiIjMDAwMDAwIiwiYmdjIjoiI0ZGRkZGRiIsInQiOjF9/S0NO/aquire.png"
         alt="KCN"
-      />
-      <div>
+      /> */}
         <img
-          src="https://see.fontimg.com/api/renderfont4/YzE0o/eyJyIjoiZnMiLCJoIjoxOCwidyI6MTUwMCwiZnMiOjEyLCJmZ2MiOiIjMDAwMDAwIiwiYmdjIjoiI0ZGRkZGRiIsInQiOjF9/S2F0ZXJpbmEg/aquire-light.png"
+          src="https://see.fontimg.com/api/renderfont4/BW0ox/eyJyIjoiZnMiLCJoIjozOSwidyI6MTUwMCwiZnMiOjI2LCJmZ2MiOiIjMDAwMDAwIiwiYmdjIjoiI0ZGRkZGRiIsInQiOjF9/S2F0ZXJpbmEg/aquire.png"
           alt="Katerina"
         />
+      <div>
         <img
           src="https://see.fontimg.com/api/renderfont4/YzE0o/eyJyIjoiZnMiLCJoIjoxOCwidyI6MTUwMCwiZnMiOjEyLCJmZ2MiOiIjMDAwMDAwIiwiYmdjIjoiI0ZGRkZGRiIsInQiOjF9/Q2hyeXNvc3RvbW91IA/aquire-light.png"
           alt="Chrysostomou"
